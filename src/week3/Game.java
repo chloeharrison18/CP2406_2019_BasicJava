74f40a6ebe77d916be1e10a2a8c4a9bf7e1290db
chloeharrison18 <chloe.harrison@my.jcu.edu.au>
2019-08-16 00:00:44 +1000

    Implemented and tested the guessing game class

---
+++ src/week3/Game.java
@@ -0,0 +1,20 @@
+package week3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    private Random random = new Random();
+    int secret = random.nextInt(10) + 1; // secret is assigned a random value between 1-10
+
+    int count;
+    int wins;
+
+    void makeGuess(int value) {
+        // supposed to check wins and add counts.
+        count = + 1;
+        if (value == secret) {
+            wins = + 1;
+        }
+    }
+}
